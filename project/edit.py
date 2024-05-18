4605c275ed6e43ebd2e57f8547c72bd45f980a37
EuleF <vlad.krabik2@gmail.com>
2024-05-18 20:38:23 +0300

    update

--- project/edit.py
+++ project/edit.py
@@ -7,11 +7,11 @@ def editplanets(mainlist):
         print("No planets found")
         return
 
-    index = 0
     while True:
         print("Input index planet to edit: ")
+        user_input = input()
         try:
-            index = int(input())
+            index = int(user_input)
             if index < 0:
                 os.system('cls')
                 print("Invalid input")
@@ -19,6 +19,7 @@ def editplanets(mainlist):
         except ValueError:
             os.system('cls')
             print("Invalid input")
+            continue
 
         if index > len(mainlist) - 1:
             os.system('cls')
@@ -28,10 +29,10 @@ def editplanets(mainlist):
             break
 
     os.system('cls')
-    print("Current planet: ")
-    print(mainlist[index])
+
     while True:
-        os.system('cls')
+        print("Current planet: ")
+        print(mainlist[index])
         print("1. Edit name")
         print("2. Edit type")
         print("3. Edit mass")
@@ -43,50 +44,64 @@ def editplanets(mainlist):
         if choice == "1":
             os.system('cls')
             print("Input new name: ")
-            mainlist[index][0] = input()
+            new_name = input()
+            if not new_name.strip():
+                os.system('cls')
+                print("Invalid input")
+                continue
+            mainlist[index][0] = new_name
             break
         elif choice == "2":
             os.system('cls')
             print("Input new type: ")
-            mainlist[index][1] = input()
+            new_type = input()
+            if not new_type.strip():
+                os.system('cls')
+                print("Invalid input")
+                continue
+            mainlist[index][1] = new_type
             break
         elif choice == "3":
             while True:
                 try:
                     os.system('cls')
                     print("Input new mass: ")
-                    mainlist[index][2] = float(input())
+                    new_mass = float(input())
                     break
                 except ValueError:
                     os.system('cls')
                     print("Invalid input")
                     continue
+            mainlist[index][2] = new_mass
             break
         elif choice == "4":
             while True:
                 try:
                     os.system('cls')
                     print("Input new temperature: ")
-                    mainlist[index][3] = float(input())
+                    new_temp = float(input())
                     break
                 except ValueError:
                     os.system('cls')
                     print("Invalid input")
                     continue
+            mainlist[index][3] = new_temp
             break
         elif choice == "5":
             while True:
                 try:
                     os.system('cls')
                     print("Input new radius: ")
-                    mainlist[index][4] = float(input())
+                    new_radius = float(input())
                     break
                 except ValueError:
                     os.system('cls')
                     print("Invalid input")
                     continue
+            mainlist[index][4] = new_radius
             break
         elif choice == "6":
+            os.system('cls')
             return
         else:
             os.system('cls')
